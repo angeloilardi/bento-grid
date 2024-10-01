157a13277d610bc5eee3085144fb74feaafd4493
Angelo Ilardi <ange448@gmail.com>
2024-10-01 04:07:38 +0100

    breakpoint change

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import aicontent from "/public/images/illustration-ai-content.webp"
 
 export default function Home() {
   return (
-    <div className="grid px-4 py-10 gap-8 lg:grid-cols-4 lg:grid-rows-10 lg:gap-8  mx-auto xl:max-w-6xl md:grid-cols-8  md:h-fit md:gap-4 overflow-hidden  md:grid-rows-[35%_45%_20%] [@media(min-height:1200px)]:max-h-screen">
+    <div className="grid px-4 py-10 gap-8 xl:grid-cols-4 xl:grid-rows-10 xl:gap-8  mx-auto xl:max-w-6xl md:grid-cols-8  md:h-fit md:gap-4 overflow-hidden  md:grid-rows-[35%_45%_20%] [@media(min-height:1200px)]:max-h-screen">
       {/* 01 */}
-      <div className="rounded-lg bg-purple-500 p-10 justify-items-center grid lg:col-start-2 lg:col-span-2 lg:row-span-4 lg:row-start-1 md:col-span-4 md:row-span-1">
+      <div className="rounded-lg bg-purple-500 p-10 justify-items-center grid xl:col-start-2 xl:col-span-2 xl:row-span-4 xl:row-start-1 md:col-span-4 md:row-span-1">
         <p className="text-white text-5xl font-medium text-center">
           Social Media <span className="text-yellow-500">10x</span>{" "}
           <span className="italic">Faster</span> with AI
@@ -19,7 +19,7 @@ export default function Home() {
         <p className="text-white text-lg">Over 4,000 5-star reviews</p>
       </div>
       {/* 02 */}
-      <div className="bg-white rounded-lg p-4 grid gap-4 lg:row-start-5 lg:row-span-3 lg:col-start-2 lg:col-span-1 md:row-start-1 md:col-span-2 md:col-start-5 ">
+      <div className="bg-white rounded-lg p-4 grid gap-4 xl:row-start-5 xl:row-span-3 xl:col-start-2 xl:col-span-1 md:row-start-1 md:col-span-2 md:col-start-5 ">
         <Image
           src="/images/illustration-multiple-platforms.webp"
           alt={""}
@@ -31,7 +31,7 @@ export default function Home() {
         </p>
       </div>
       {/* 03 */}
-      <div className="bg-yellow-500 px-4 pt-4 grid rounded-lg gap-4 lg:row-start-5 lg:row-span-3 overflow-hidden lg:col-start-3 lg:col-span-1 md:col-start-5 md:col-span-4 md:row-start-3">
+      <div className="bg-yellow-500 px-4 pt-4 grid rounded-lg gap-4 xl:row-start-5 xl:row-span-3 overflow-hidden xl:col-start-3 xl:col-span-1 md:col-start-5 md:col-span-4 md:row-start-3">
         <p className="text-black text-3xl font-medium">
           Maintain a consistent posting schedule.
         </p>
@@ -43,7 +43,7 @@ export default function Home() {
         />
       </div>
       {/* 04 */}
-      <div className="bg-purple-100 rounded-lg p-4 lg:gap-3 gap-6 grid justify-items-center text-center lg:col-start-4 lg:row-span-7 lg:col-span-1 lg:row-start-1 md:col-start-4 md:col-span-3 lg:text-left">
+      <div className="bg-purple-100 rounded-lg p-4 xl:gap-3 gap-6 grid justify-items-center text-center xl:col-start-4 xl:row-span-7 xl:col-span-1 xl:row-start-1 md:col-start-4 md:col-span-3 xl:text-left">
         <p className="text-black text-3xl font-medium">
           Schedule to social media.
         </p>
@@ -61,7 +61,7 @@ export default function Home() {
         </p>
       </div>
       {/* 05 */}
-      <div className="bg-purple-500 rounded-lg p-6 grid md:grid-cols-2 justify-items-center gap-10 lg:col-start-3 lg:col-span-2 lg:row-span-3 md:grid-flow-col lg:row-start-8 md:col-span-4 md:col-start-1">
+      <div className="bg-purple-500 rounded-lg p-6 grid md:grid-cols-2 justify-items-center gap-10 xl:col-start-3 xl:col-span-2 xl:row-span-3 md:grid-flow-col xl:row-start-8 md:col-span-4 md:col-start-1">
         <Image
           src="/images/illustration-grow-followers.webp"
           alt={""}
@@ -73,7 +73,7 @@ export default function Home() {
         </p>
       </div>
       {/* 06 */}
-      <div className="bg-white rounded-lg p-6 flex flex-col gap-5 lg:row-start-8 lg:row-span-3 lg:col-start-2 lg:col-span-1 md:col-span-2 md:col-start-7 md:row-start-2">
+      <div className="bg-white rounded-lg p-6 flex flex-col gap-5 xl:row-start-8 xl:row-span-3 xl:col-start-2 xl:col-span-1 md:col-span-2 md:col-start-7 md:row-start-2">
         <p className="text-black text-4xl font-medium">{">56%"}</p>
         <p className="text-black">faster audience growth</p>
         <Image
@@ -84,7 +84,7 @@ export default function Home() {
         />
       </div>
       {/* 07 */}
-      <div className="bg-yellow-100 rounded-lg grid p-8 gap-5 lg:row-start-1 lg:row-span-5 lg:col-span-1 lg:col-start-1 md:col-start-7 md:row-start-1 md:col-span-2">
+      <div className="bg-yellow-100 rounded-lg grid p-8 gap-5 xl:row-start-1 xl:row-span-5 xl:col-span-1 xl:col-start-1 md:col-start-7 md:row-start-1 md:col-span-2">
         <p className="text-black text-3xl font-medium">
           Create and schedule content{" "}
           <span className="italic text-purple-500">quicker.</span>
@@ -97,7 +97,7 @@ export default function Home() {
         />
       </div>
       {/* 08 */}
-      <div className="bg-yellow-500 rounded-lg flex flex-col p-6 lg:col-start-1 lg:col-span-1 lg:row-start-6 lg:row-span-5 gap-6 overflow-hidden  md:col-start-1 md:col-span-3 md:row-start-2">
+      <div className="bg-yellow-500 rounded-lg flex flex-col p-6 xl:col-start-1 xl:col-span-1 xl:row-start-6 xl:row-span-5 gap-6 overflow-hidden  md:col-start-1 md:col-span-3 md:row-start-2">
         <p className="text-black text-3xl font-medium">
           Write your content using AI.
         </p>
